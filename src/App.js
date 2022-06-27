64e72d6e4c31e4fb86b102794fb87914fcf9b35b
MoustafaRagab1 <88005666+MoustafaRagab1@users.noreply.github.com>
2022-06-27 13:14:30 +0400

    Upload The Design

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
+import React , {useState} from 'react'
 import './App.css';
-import img from './image-header-desktop.jpg'
+import Pricing from './components/Pricing';
+import Data from './components/Data.json';
+import Card from './components/Card'
 
-function App() {
+const App = () => {
+
+  const [choice, setChoice] = useState(Data)
+  const [isAnnual, setIsAnnual] = useState(true)
+  
   return (
-    <div className="card">
-      <div className="img">
-        <img src={img} alt="" />
-
+    <div className="container">
+      <Pricing 
+      setChoice={setChoice} 
+      setIsAnnual={setIsAnnual} 
+      isAnnual={isAnnual}
+      />
+      <div className="card-container">
+        {choice.map(data => (
+          <Card key={data.id} data={data}/>
+        ))}
       </div>
-      <div className="body">
-
-        <h1 className="header">Get <span>insights</span> that help your business grow.</h1>
-        <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aliquam minus nobis placeat distinctio perferendis, soluta eveniet non, </p>
-
-        <div className="detailes">
-
-          <div className="nums">
-            10k+ 
-            <div>companies</div>
-          </div>
-          <div className="nums">
-            314 
-            <div>tamplates</div>
-          </div>
-          
-          <div className="nums">
-            12M+ 
-            <div>queries</div>
-          </div>
-          
-
-        </div>
-      </div>
-
     </div>
+   
   );
 }
 
